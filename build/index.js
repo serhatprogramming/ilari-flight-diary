b6dda03f0999e6a497db2310b644d0310d888008
serhatprogramming <serhatprogramming@gmail.com>
2023-05-12 09:57:09 -0400

    ignorePatterns added for build folder

--- build/index.js
+++ build/index.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
@@ -12,6 +10,6 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = 3000;
 app.get("/ping", (_req, res) => {
-  res.send("pong");
+    res.send("pong");
 });
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
