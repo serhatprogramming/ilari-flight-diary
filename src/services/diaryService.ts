56c72b24eb23780171690f24b54e53a06b363a94
serhatprogramming <serhatprogramming@gmail.com>
2023-05-13 15:14:02 -0400

    add a new diary added

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -2,7 +2,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import diaries from "../../data/entries";
 
-import { DiaryEntry, NonSensitiveDiaryEntry } from "../types";
+import {
+  DiaryEntry,
+  NonSensitiveDiaryEntry,
+  Visibility,
+  Weather,
+} from "../types";
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
@@ -22,8 +27,23 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addDiary = () => {
-  return null;
+const addDiary = (
+  date: string,
+  weather: Weather,
+  visibility: Visibility,
+  comment: string
+): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    date,
+    weather,
+    visibility,
+    comment,
+  };
+
+  diaries.push(newDiaryEntry);
+
+  return newDiaryEntry;
 };
 
 export default { getEntries, addDiary, getNonSensitiveEntries, findById };
